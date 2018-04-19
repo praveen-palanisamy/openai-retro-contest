873ec7a3aff76f7592984db367f5f4531202a353
Praveen Palanisamy <praveen.palanisamy@gm.com>
2018-04-19 17:14:25 -0400

    Renamed the train/test config generator utility script

--- utils/generate_train_test_conf.py
+++ utils/generate_train_test_conf.py
@@ -23,15 +23,21 @@ sonic_val = pd.read_csv("sonic-validation.csv", header=0)
 
 if args.game is not 'ALL':
     # input is not sanity checked
-    sonic_train = sonic_train.where(sonic_train.game == args.game)
-    sonic_val = sonic_val.where(sonic_val.game == args.game)
+    sonic_train = sonic_train.where(sonic_train.game == args.game).dropna()
+    sonic_val = sonic_val.where(sonic_val.game == args.game).dropna()
 
-sonic_train_conf = sonic_train.apply(lambda row,: "{game:'" + row['game'] + "',level:'" +
+sonic_train_conf = sonic_train.apply(lambda row: "{'game':'" + row['game'] + "','level':'" +
                                               row['state'] + "'}", axis=1, ignore_failures=True)
-sonic_val_conf = sonic_val.apply(lambda row,: "{game:'" + row['game'] + "',level:'" +
+sonic_val_conf = sonic_val.apply(lambda row: "{'game':'" + row['game'] + "','level':'" +
                                               row['state'] + "'}", axis=1, ignore_failures=True)
 
-sonic_conf= {"Train": dict(sonic_train_conf), "Test": dict(sonic_val_conf) }
+train_conf = { k: eval(v) for k,v in dict(sonic_train_conf).items()}  # rm quotes from each value
+test_conf = { k: eval(v) for k,v in dict(sonic_val_conf).items()}
+
+sonic_conf = {"Spaces":{"observation_channels": 1 , "action_shape": 7},
+              "Train": train_conf,
+              "Test": test_conf }
+
 output_file = open("sonic_config.json", 'w')
 json.dump(sonic_conf, fp=output_file, indent=4)
 
